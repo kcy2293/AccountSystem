3f60f8f5b0bb4c61a3eb5a06df1f006648483036
kcy2293 <kcy2293.nate.com>
2016-07-08 00:05:48 +0900

    init
    init

---
+++ server.js
@@ -0,0 +1,37 @@
+global.env = process.env.NODE_ENV || 'production';
+/*******************
+ * modules
+ *******************/
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+/*******************
+ * setup express
+ *******************/
+var app = express();
+var port = '3000';
+app.set('port', port);
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(favicon(path.join(__dirname, 'views', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'views')));
+/*******************
+ * routing
+ *******************/
+ app.get('/', function(req, res) {
+   res.redirect('/index.html');
+ });
+
+/*******************
+ * http server open
+ *******************/
+var httpServer = http.createServer(app);
+httpServer.listen(port, function() {
+  console.log("HTTP Server listening on port " + app.get('port'));
+});
---
+++ views/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('myApp', [
+  'ngMaterial'
+]);
+/*
+angular.module('myApp', [
+  'ngRoute', 'ngMaterial'
+]).config(function($stateProvider, $routeProvider) {
+
+  $stateProvider.state('app', {
+    url: '/',
+    templateUrl: 'app/app.html',
+    controller: 'AppCtrl'
+  });
+}).run(function() {
+
+});
+*/
