44988e3ecfa9848e595701c0d88ccf7f134f4e98
kcy2293 <kcy2293@nate.com>
2016-08-30 23:01:29 +0900

    window mongorestore

--- dump.sh
+++ dump.sh
@@ -5,3 +5,6 @@ mongodump --port 9000 -o real_dump
 mongodump
 mongorestore
 mongorestore real_dump
+
+## windows
+e:\dev\MongoDB\Server\3.2\bin\mongorestore.exe -h localhost e:\dev\AccountSystem\real_dump
